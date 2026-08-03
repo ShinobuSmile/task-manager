aa1b348feda99994a0b5b5f9eb604952e0b25ac3
ShinobuSmile <antopro999@gmail.com>
2026-08-03 20:17:31 +0200

    added ability to delete a specific task

--- app/tasks/router.py
+++ app/tasks/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Query
+from fastapi import APIRouter, Depends, status, HTTPException, Query, Response
 from datetime import date
 from sqlalchemy.orm import Session
 from typing import Optional
@@ -62,3 +62,14 @@ def update_task(task_id:int, task_update: TaskUpdate, current_user: User = Depen
     db.commit()
     db.refresh(task)
     return task
+
+
+#delete task
+@router.delete("/{task_id}")
+def delete_task(task_id:int, current_user: User = Depends(get_current_user), db:Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.user_id == current_user.id, Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NO TASK FOUND")
+    db.delete(task)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
